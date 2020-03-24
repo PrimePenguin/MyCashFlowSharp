48d10aeb95f2cacdfd663048e3fceac43f91eb36
khungersumit <36700687+Khungersumit@users.noreply.github.com>
2020-03-24 18:58:01 +0530

    Update build.ps1

--- scripts/build.ps1
+++ scripts/build.ps1
@@ -1,8 +1,8 @@
 dotnet restore;
 dotnet build -c Release;
-dotnet pack --no-build -c Release MyCashFlowSharp/MyCashFlowSharp.csproj;
+dotnet pack --no-build -c Release MyCashFlowSharp/MyCashFlowSharp/MyCashFlowSharp.csproj;
 
-$nupkg = (Get-ChildItem MyCashFlowSharp/bin/Release/*.nupkg)[0];
+$nupkg = (Get-ChildItem MyCashFlowSharp/MyCashFlowSharp/bin/Release/*.nupkg)[0];
 
 # Push the nuget package to AppVeyor's artifact list.
 Push-AppveyorArtifact $nupkg.FullName -FileName $nupkg.Name -DeploymentName "MyCashFlowSharp.nupkg";
